3bb8867d50e8eafef53b024f5da684e681466cb5
Mario Krstevski <mariokrstevski@hotmail.com>
2024-01-11 00:46:26 +0100

    norria config values

--- global/variables.ts
+++ global/variables.ts
@@ -1,5 +1,5 @@
 const defaultStoreId = "8aacb4bd-6b25-4500-aa2f-71eaa77e9abe"; // Suzana Kroevi
 const defaultBillboardId = "8324be14-61c1-4281-b96e-2045e56c84cf"; // Vencanici
 
-export const storeId = "6ef36afb-904b-473d-bce6-05b84d695339"; //Fitness
-export const billboardId = "b49f4b07-bfac-4648-8fe0-21206815e3cd"; //protein shake
+export const storeId = "d87ce47d-8841-479d-87f6-a39789c888c2"; //Cat Store
+export const billboardId = "ba07658c-2247-4e9e-8144-ed89a3b2a81a"; //Puff
